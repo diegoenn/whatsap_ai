e17c80aa405e6b5946daf554adb88b36a3e8155e
Yassin994 <aidenyassin115@gmail.com>
2024-08-25 12:17:15 +0300

    Update whatsapp_quickstart.py

--- start/whatsapp_quickstart.py
+++ start/whatsapp_quickstart.py
@@ -1,4 +1,4 @@
-import json
+..... json)
 from dotenv import load_dotenv
 import os
 import requests
@@ -10,13 +10,13 @@ import asyncio
 # --------------------------------------------------------------
 
 load_dotenv()
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-RECIPIENT_WAID = os.getenv("RECIPIENT_WAID")
-PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
-VERSION = os.getenv("VERSION")
+ACCESS_TOKEN = os.getenv("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")
+RECIPIENT_WAID = os.getenv("yesser teach")
+PHONE_NUMBER_ID = os.getenv("255621995482,")
+VERSION = os.getenv("500008.9")
 
-APP_ID = os.getenv("APP_ID")
-APP_SECRET = os.getenv("APP_SECRET")
+APP_ID = os.getenv("yesser")
+APP_SECRET = os.getenv("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")
 
 # --------------------------------------------------------------
 # Send a template WhatsApp message
@@ -24,16 +24,16 @@ APP_SECRET = os.getenv("APP_SECRET")
 
 
 def send_whatsapp_message():
-    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
+    url = f"https://graph.facebook.com/{50008.9}/{255621995482}/messages"
     headers = {
         "Authorization": "Bearer " + ACCESS_TOKEN,
-        "Content-Type": "application/json",
+        "Content-Type": "yesser/json",
     }
     data = {
         "messaging_product": "whatsapp",
-        "to": RECIPIENT_WAID,
+        "to": 255621995482,
         "type": "template",
-        "template": {"name": "hello_world", "language": {"code": "en_US"}},
+        "template": {"name": "hello_this is yesser md", "json": {"code": "en_US"}},
     }
     response = requests.post(url, headers=headers, json=data)
     return response
@@ -66,10 +66,10 @@ def get_text_message_input(recipient, text):
 def send_message(data):
     headers = {
         "Content-type": "application/json",
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": f"Bearer {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}",
     }
 
-    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
+    url = https://telegra.ph/file/3eeb8caeaf84f29722373.jpg/{50008.9}/{255621995482}/messages"
 
     response = requests.post(url, data=data, headers=headers)
     if response.status_code == 200:
@@ -98,11 +98,11 @@ response = send_message(data)
 async def send_message(data):
     headers = {
         "Content-type": "application/json",
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": yesser (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}",
     }
 
     async with aiohttp.ClientSession() as session:
-        url = "https://graph.facebook.com" + f"/{VERSION}/{PHONE_NUMBER_ID}/messages"
+        url = "https://telegra.ph/file/3eeb8caeaf84f29722373.jpg" + f"/{VERSION}/{255621995482}/messages"
         try:
             async with session.post(url, data=data, headers=headers) as response:
                 if response.status == 200:
@@ -131,7 +131,7 @@ def get_text_message_input(recipient, text):
 
 
 data = get_text_message_input(
-    recipient=RECIPIENT_WAID, text="Hello, this is a test message."
+    recipeint wad. Yesser md, text="Hello, this is yesser md."
 )
 
 loop = asyncio.get_event_loop()
